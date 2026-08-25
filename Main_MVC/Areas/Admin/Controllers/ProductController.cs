d42db7a2ad793b4980da129a6285cfef1f31fd18
Atnameh <hemantaneupane76@gmail.com>
2026-08-25 13:49:09 +0545

    Learned to add photo for the project in the database by keeping the image in static folder and providing the address of the stored location

--- Main_MVC/Areas/Admin/Controllers/ProductController.cs
+++ Main_MVC/Areas/Admin/Controllers/ProductController.cs
@@ -1,5 +1,8 @@
 using Azure;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.Extensions.FileProviders;
 using MVC.DataAccess.Repository.IRepository;
 using MVC.Models.Models;
 
@@ -9,10 +12,12 @@ namespace Main_MVC.Areas.Admin.Controllers
     public class ProductController : Controller
     {
         private readonly IUnitOfWork _Repo;
+        private readonly IWebHostEnvironment _WebHostEnviroment;
 
-        public ProductController(IUnitOfWork db)
+        public ProductController(IUnitOfWork db, IWebHostEnvironment webHostEnvironment)
         {
             _Repo = db;
+            _WebHostEnviroment = webHostEnvironment;
         }
         public IActionResult Index()
         {
@@ -22,34 +27,80 @@ namespace Main_MVC.Areas.Admin.Controllers
 
         public IActionResult Create()
         {
+            IEnumerable<SelectListItem> CategoryList = _Repo.category
+            .GetAll().Select(u => new SelectListItem
+            {
+                Text = u.Name,
+                Value = u.Id.ToString()
+            });
+            ViewBag.CategoryList = CategoryList;
+
             return View();
         }
 
         [HttpPost]
-        public IActionResult Create(Product obj)
+        public IActionResult Create(Product obj, IFormFile file)
         {
-            if (ModelState.IsValid)
+            string wwwRootPath = _WebHostEnviroment.WebRootPath;
+            if (file != null)
             {
-                _Repo.product.Add(obj);
-                _Repo.Save();
-                return RedirectToAction("Index");
+                string filename = Guid.NewGuid().ToString() + Path.GetExtension(file.FileName);
+                string productPath = Path.Combine(wwwRootPath, @"Images/Product");
+
+                using (var filestream = new FileStream(Path.Combine(productPath, filename), FileMode.Create))
+                {
+                    file.CopyTo(filestream);
+                }
+                obj.ImageUrl = @"/Images/Product/" + filename;
             }
-            return View();
+            _Repo.product.Add(obj);
+            _Repo.Save();
+            TempData["success"] = "Product Created successfully";
+            return RedirectToAction("Index");
         }
 
         public IActionResult Edit(int id)
         {
+            IEnumerable<SelectListItem> CategoryList = _Repo.category
+            .GetAll().Select(u => new SelectListItem
+            {
+                Text = u.Name,
+                Value = u.Id.ToString()
+            });
+            ViewBag.CategoryList = CategoryList;
             Product obj = _Repo.product.GetValue(obj => obj.Id == id);
             return View(obj);
         }
 
         [HttpPost]
-        public IActionResult Edit(Product obj)
+        public IActionResult Edit(Product obj, IFormFile? file)
         {
             if (ModelState.IsValid)
             {
+                string wwwRootPath = _WebHostEnviroment.WebRootPath;
+                if (file != null)
+                {
+                    string filename = Guid.NewGuid().ToString() + Path.GetExtension(file.FileName);
+                    string productPath = Path.Combine(wwwRootPath, @"Images/Product");
+
+                    if (!string.IsNullOrEmpty(obj.ImageUrl))
+                    {
+                        var oldPhotoPath = Path.Combine(wwwRootPath, obj.ImageUrl.TrimStart('/'));
+                        if (System.IO.File.Exists(oldPhotoPath))
+                        {
+                            System.IO.File.Delete(oldPhotoPath);
+                        }
+                    }
+
+                    using (var filestream = new FileStream(Path.Combine(productPath, filename), FileMode.Create))
+                    {
+                        file.CopyTo(filestream);
+                    }
+                    obj.ImageUrl = @"/Images/Product/" + filename;
+                }
                 _Repo.product.Update(obj);
                 _Repo.Save();
+                TempData["success"] = "Product Updated successfully";
                 return RedirectToAction("Index");
             }
             return View();
@@ -66,6 +117,7 @@ namespace Main_MVC.Areas.Admin.Controllers
         {
             _Repo.product.Remove(obj);
             _Repo.Save();
+            TempData["success"] = "Product Deleted successfully";
             return RedirectToAction("Index");
 
         }
